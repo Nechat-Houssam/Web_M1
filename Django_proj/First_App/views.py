f576ea30e73b47a31d4f33b78693522570ff6340
Nechat-Houssam <houssam.nechat@dauphine.eu>
2023-03-18 22:39:05 +0100

    views

--- Django_proj/First_App/views.py
+++ Django_proj/First_App/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+def login_page(request):
+
+    return render(request, 'login.html')
+
+def main_page(request):
+  
+    return render(request, 'main.html')
+
+def admin_view(request):
+  
+    return render(request, 'admin.html')
+
+def info_page(request):
+    
+    return render(request, 'info.html')
