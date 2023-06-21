cfc83d23e461dfec4730fcff30d738bdecd5fb0f
SauceAigreDouce <momolaterreurdu77@gmail.com>
2023-06-21 03:57:49 +0200

    English traduction

--- RoomMate/RoomMate/RoomManager/admin.py
+++ RoomMate/RoomMate/RoomManager/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import UserProfile, Room, Event
+from .models import UserProfile, Room, Event, EventInvite, EventRequest
 
 admin.site.register(Room)
 admin.site.register(Event)
 admin.site.register(UserProfile)
+admin.site.register(EventRequest)
+admin.site.register(EventInvite)
